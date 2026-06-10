8c3f9d4aaca13035eb66eab933a4bf7c80df3c1c
shakshi <singhshakshi327@gmail.com>
2026-06-11 00:31:32 +0530

    signup

--- routes/listing.js
+++ routes/listing.js
@@ -26,6 +26,11 @@ router.get(
 
 //NEW ROUTE
 router.get("/new", (req, res) => {
+  console.log(req.user);
+  if (!req.isAuthenticated()) {
+    req.flash("error", "You must be signed in to create a listing!");
+    return res.redirect("/login");
+  }
   res.render("listings/new.ejs");
 });
 
